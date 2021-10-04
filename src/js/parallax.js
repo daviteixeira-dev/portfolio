dcc190d0bff7d137715f32e4239d512395d9a0ab
Davi Teixeira <daviteixeira.btm@gmail.com>
2021-10-04 00:56:03 -0300

    tentando colocar o alt no parallax

--- src/js/parallax.js
+++ src/js/parallax.js
@@ -1,5 +1,5 @@
 // Parallax
 setTimeout(function(){
-    $('#banner').parallax({imageSrc: '../src/img/services-banner.webp'});
+    $('#banner').parallax({imageSrc: '../src/img/services-banner.webp', alt: 'banner do site'});
     $('#thanks-banner').parallax({imageSrc: '../src/img/services-banner.webp'});  
 }, 250);
