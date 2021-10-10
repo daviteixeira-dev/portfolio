f460f4fd78da429ab4fc1164021503a6b8ecd3ce
Davi Teixeira <daviteixeira.btm@gmail.com>
2021-10-10 00:58:08 -0300

    Ajuste no sw.js

--- sw.js
+++ sw.js
@@ -19,55 +19,32 @@ var urlsToCache = [
 ];
 
 //Adding `install` event listener
-self.addEventListener('install', function(event){
-    console.info('Event: Install');
-    // Perform install steps
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-          .then(function(cache){
-            console.log('Opened cache');
-            return cache.addAll(urlsToCache);
-          })
-    );
+self.addEventListener('install', (event) => {
+    event.waitUntil(caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+    )
 });
-
-self.addEventListener('fetch', function(event){
-    event.respondWith(
-        caches.match(event.request)
-            .then(function(response) {
-            // Cache hit - return response
-            if(response){
-                return response;
-            }
   
-            // IMPORTANT: Clone the request. A request is a stream and
-            // can only be consumed once. Since we are consuming this
-            // once by cache and once by the browser for fetch, we need
-            // to clone the response.
-            var fetchRequest = event.request.clone();
+self.addEventListener('activate', (event) => {
+    const cacheWhiteList = ['pwa-task-manager'];
+    event.waitUntil(caches.keys()
+      .then((cacheNames) => Promise.all(cacheNames.map((cacheName) => {
+        if (cacheWhiteList.indexOf(cacheName) === -1) {
+          return caches.delete(cacheName);
+        }
+      })))
+    )
+});
   
-            return fetch(fetchRequest).then(
-                function(response) {
-                // Check if we received a valid response
-                if(!response || response.status !== 200 || response.type !== 'basic'){
-                    return response;
-                }
-  
-                // IMPORTANT: Clone the response. A response is a stream
-                // and because we want the browser to consume the response
-                // as well as the cache consuming the response, we need
-                // to clone it so we have two streams.
-                var responseToCache = response.clone();
-  
-                caches.open(CACHE_NAME)
-                    .then(function(cache) {
-                        cache.put(event.request, responseToCache);
-                    });
-
-                    return response;
-                }
-            );
-        })
-    );
-});  
-  
+self.addEventListener('fetch', (event) => {
+    event.respondWith(caches.open(CACHE_NAME)
+      .then((cache) => cache.match(event.request)
+        .then((response) => response || fetch(event.request)
+          .then((res) => {
+            cache.put(event.request, res.clone());
+            return res;
+          })
+        )
+      )
+    )
+});
