7427b013aa92b5fc9db98daf2ecce6afb251dfc0
Davi Teixeira <daviteixeira.btm@gmail.com>
2021-11-19 18:37:35 -0300

    Alterando o sw, manifest e scripts.js

--- src/js/sw.js
+++ src/js/sw.js
@@ -1,50 +1,52 @@
-var CACHE_NAME = 'my-site-cache-v1';
-
-// Files to save in cache
-var urlsToCache = [
-  /* Add an array of files to precache for your app */
-  './',
-  './src/img/services-banner.webp',
-  './src/img/main-banner.webp',
-  './src/img/logo.png',
-  './src/img/banner-cards/about-me.webp',
-  './src/img/banner-cards/contact.webp',
-  './src/img/banner-cards/services.webp',
-  './src/img/banner-cards/projects.webp',
-  './src/css/style.css',
-  './src/img/banner-cards/studies.webp',
-  './src/img/banner-cards/my-blog.webp',
-  './src/js/scripts.js',
-  './src/js/parallax.js'
-];
+var myCacheName = 'daviteixeira-dev-cache-v1';
 
 //Adding `install` event listener
-self.addEventListener('install', (event) => {
-    event.waitUntil(caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
-    )
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(myCacheName)
+    .then(function (cache) {
+      cache.addAll([
+        './',
+        './index.html',
+        './src/js/sw.js',
+        './src/img/logo.png',
+        './pages/thanks.html',
+        './src/css/style.css',
+        './src/js/scripts.js',
+        './src/js/manifest.json',
+        './src/img/main-banner.webp',
+        './src/img/services-banner.webp',
+        './src/img/banner-cards/studies.webp',
+        './src/img/banner-cards/my-blog.webp',
+        './src/img/banner-cards/contact.webp',
+        './src/img/banner-cards/about-me.webp',
+        './src/img/banner-cards/services.webp',
+        './src/img/banner-cards/projects.webp'
+        ]);
+      })
+    );
 });
-  
+
 self.addEventListener('activate', (event) => {
-    const cacheWhiteList = ['pwa-task-manager'];
-    event.waitUntil(caches.keys()
-      .then((cacheNames) => Promise.all(cacheNames.map((cacheName) => {
-        if (cacheWhiteList.indexOf(cacheName) === -1) {
-          return caches.delete(cacheName);
-        }
-      })))
-    )
+  const cacheWhiteList = ['pwa-task-manager'];
+  event.waitUntil(caches.keys()
+    .then((cacheNames) => Promise.all(cacheNames.map((cacheName) => {
+      if (cacheWhiteList.indexOf(cacheName) === -1) {
+        return caches.delete(cacheName);
+      }
+    })))
+  )
 });
-  
+
 self.addEventListener('fetch', (event) => {
-    event.respondWith(caches.open(CACHE_NAME)
-      .then((cache) => cache.match(event.request)
-        .then((response) => response || fetch(event.request)
-          .then((res) => {
-            cache.put(event.request, res.clone());
-            return res;
-          })
-        )
+  event.respondWith(caches.open(myCacheName)
+    .then((cache) => cache.match(event.request)
+      .then((response) => response || fetch(event.request)
+        .then((res) => {
+          cache.put(event.request, res.clone());
+          return res;
+        })
       )
     )
+  )
 });
