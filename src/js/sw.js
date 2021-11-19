968a8cf08cb549e6bfaa1b9914ea8db59bf0a7bb
Davi Teixeira <daviteixeira.btm@gmail.com>
2021-11-19 19:07:17 -0300

    ajustando o sw e o manifest

--- src/js/sw.js
+++ src/js/sw.js
@@ -1,31 +1,31 @@
 var myCacheName = 'daviteixeira-dev-cache-v1';
 
 //Adding `install` event listener
-self.addEventListener('install', function (event) {
-  event.waitUntil(
-    caches.open(myCacheName)
-    .then(function (cache) {
-      cache.addAll([
-        './',
-        './index.html',
-        './src/js/sw.js',
-        './src/img/logo.png',
-        './pages/thanks.html',
-        './src/css/style.css',
-        './src/js/scripts.js',
-        './src/js/manifest.json',
-        './src/img/main-banner.webp',
-        './src/img/services-banner.webp',
-        './src/img/banner-cards/studies.webp',
-        './src/img/banner-cards/my-blog.webp',
-        './src/img/banner-cards/contact.webp',
-        './src/img/banner-cards/about-me.webp',
-        './src/img/banner-cards/services.webp',
-        './src/img/banner-cards/projects.webp'
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches.open('myCacheName')
+      .then((cache) => {
+        return cache.addAll([
+          './',
+          './index.html',
+          './src/js/sw.js',
+          './src/img/logo.png',
+          './pages/thanks.html',
+          './src/css/style.css',
+          './src/js/scripts.js',
+          './src/js/manifest.json',
+          './src/img/main-banner.webp',
+          './src/img/services-banner.webp',
+          './src/img/banner-cards/studies.webp',
+          './src/img/banner-cards/my-blog.webp',
+          './src/img/banner-cards/contact.webp',
+          './src/img/banner-cards/about-me.webp',
+          './src/img/banner-cards/services.webp',
+          './src/img/banner-cards/projects.webp'
         ]);
       })
     );
-});
+  });
 
 self.addEventListener('activate', (event) => {
   const cacheWhiteList = ['pwa-task-manager'];
