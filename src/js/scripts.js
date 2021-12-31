69f2fa33ffe889d30e6fdf0c0b189ceb65d10dd5
Davi Teixeira <daviteixeira.btm@gmail.com>
2021-12-31 13:11:49 -0300

    function de botão para o topo

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -13,21 +13,10 @@ if('serviceWorker' in navigator){
 }
 
 // Button back to the top
+const buttonScroll = document.getElementById("back-to-top");
 
-$(window).scroll(function () {
-    if ($(this).scrollTop() > 50) {
-        $('#back-to-top').fadeIn();
-    } else {
-        $('#back-to-top').fadeOut();
-    }
-});    
-
-// scroll body to 0px on click
-$('#back-to-top').click(function () {
-    $('body,html').animate({
-        scrollTop: 0
-    }, 400);
-    return false;
+buttonScroll.addEventListener('click', () => {
+    window.scroll({top: 0, behavior: 'smooth'});
 });
 
 // Form Action
