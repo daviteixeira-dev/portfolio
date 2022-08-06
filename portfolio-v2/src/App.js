dee4a0304470b882b20e087d7bea16a7f5046e11
Davi Teixeira <daviteixeira.btm@gmail.com>
2022-08-06 18:11:50 -0300

    atualização do portfolio para sua versão em react-bootstrap

---
+++ portfolio-v2/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './App.css';
+import  'bootstrap/dist/css/bootstrap.min.css' ;
+
+import { Button } from 'react-bootstrap' ;
+
+function App() {
+  return (
+    <div className="App">
+        <Button>Esse é um botão</Button>
+    </div>
+  );
+}
+
+export default App;
