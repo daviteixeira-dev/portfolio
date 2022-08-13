754bdc8c94dc6cb597b1d6b87a741131825297f0
Davi Teixeira <daviteixeira.btm@gmail.com>
2022-08-13 03:24:57 -0300

    Add logo do website

--- portfolio-v2-chakra-ui/src/components/Header/Logo.jsx
+++ portfolio-v2-chakra-ui/src/components/Header/Logo.jsx
@@ -1,14 +1,21 @@
-import React from "react"
-import { Box, Text } from "@chakra-ui/react"
+import React from "react";
+import { Text, Image, Flex } from "@chakra-ui/react";
 
-function Logo(props) {
-  return (
-    <Box {...props}>
+function Logo(props){
+  return(
+    <Flex {...props} alignItems="center">
+      <Image
+        mr="1rem"
+        boxSize='3rem'
+        objectFit='cover'
+        src="images/logo.png"
+        alt='Essa é a logo do meu website'
+      />
       <Text fontSize="lg" fontWeight="bold">
         Davi Teixeira
       </Text>
-    </Box>
-  )
+    </Flex>
+  );
 };
 
 export default Logo;
