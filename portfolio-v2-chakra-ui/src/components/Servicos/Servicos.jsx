9eb739ac421a7273127ff99b8e397802f13057b8
Davi Teixeira <daviteixeira.btm@gmail.com>
2022-08-20 18:11:20 -0300

    estilização da section de serviços

--- portfolio-v2-chakra-ui/src/components/Servicos/Servicos.jsx
+++ portfolio-v2-chakra-ui/src/components/Servicos/Servicos.jsx
@@ -1,32 +1,161 @@
 import React from "react";
-import { Box, Heading, Text } from "@chakra-ui/react"
+import { Flex, Heading, Text } from "@chakra-ui/react";
+
+import { FaCode, FaTabletAlt, FaCogs } from "react-icons/fa";
 
 function Servicos(){
     return(
-        <Box as="section" id="services" border="1px solid">
-            <Heading as="h2">
-                Serviços
+        <Flex
+            w="100vw"
+            h="100%"
+            p="2rem"
+            bg="black"
+            as="section"
+            id="services"
+            flexDirection="column"
+            justifyContent="center"
+        >
+            <Heading
+                as="h2"
+                mb="3rem"
+                fontSize="3rem"
+                color="#f8f8f7"
+                textAlign="center"
+                fontFamily="Inter, sans-serif"
+            >
+                Serviços<Text as="span" color="#63B3ED">.</Text>
             </Heading>
-            <Heading as="h3">
-                Desenvolvimento
-            </Heading>
-            <Text>
-                Desenvolvimento de sites profissionais, sistemas web, blogs e lojas virtuais.
-            </Text>
-            <Heading as="h3">
-                Responsividade
-            </Heading>
-            <Text>
-                Criação de sites responsivos para a melhor visualização em todas as resoluções.
-            </Text>
-            <Heading as="h3">
-                SEO
-            </Heading>
-            <Text>
-                Estratégia e monitoração para seu negócio, fazendo com que seu site apareça cada vez mais nas buscas.
-            </Text>
-        </Box>
-    )
-}
+            
+            <Flex
+                justifyContent="space-evenly"
+                flexDirection={["column", "row"]}
+            >
+
+                <Flex
+                    p="2rem"
+                    my=".5rem"
+                    bg="gray.900"
+                    w={["100%", "30vw"]}
+                    flexDirection="column"
+                    _hover={{ 
+                        border: "3px solid #63B3ED",
+                    }}
+                >
+                    <Flex
+                        justifyContent="center"
+                    >
+                        <FaCode 
+                            color="#f8f8f7" 
+                            fontSize="3rem"
+                        />
+                    </Flex>
+
+                    <Heading
+                        as="h3"
+                        my="1rem"
+                        color="#f8f8f7"
+                        textAlign="center"
+                        fontSize={["1.5rem", "2rem"]}
+                        fontFamily="Inter, sans-serif"
+                    >
+                        Desenvolvimento
+                    </Heading>
+
+                    <Text
+                        color="#f8f8f7"
+                        textAlign="center"
+                        fontFamily="Inter, sans-serif"
+                        fontSize={["1.2rem", "1.4rem"]}
+                    >
+                        Desenvolvimento de sites profissionais, sistemas web, 
+                        blogs e lojas virtuais.
+                    </Text>
+                </Flex>
+
+                <Flex 
+                    p="2rem"
+                    my=".5rem"
+                    bg="gray.900"
+                    w={["100%", "30vw"]}
+                    flexDirection="column"
+                    _hover={{ 
+                        border: "3px solid #63B3ED",
+                    }}
+                >
+                    <Flex
+                        justifyContent="center"
+                    >
+                        <FaTabletAlt
+                            color="#f8f8f7" 
+                            fontSize="3rem"
+                        />
+                    </Flex> 
+
+                    <Heading
+                        as="h3"
+                        my="1rem"
+                        color="#f8f8f7"
+                        textAlign="center"
+                        fontSize={["1.5rem", "2rem"]}
+                        fontFamily="Inter, sans-serif"
+                    >
+                        Responsividade
+                    </Heading>
+
+                    <Text
+                        color="#f8f8f7"
+                        textAlign="center"
+                        fontFamily="Inter, sans-serif"
+                        fontSize={["1.2rem", "1.4rem"]} 
+                    >
+                        Criação de sites responsivos para a melhor visualização 
+                        em todas as resoluções.
+                    </Text>
+                </Flex>
+
+                <Flex
+                    p="2rem"
+                    my=".5rem"
+                    bg="gray.900"
+                    w={["100%", "30vw"]}
+                    flexDirection="column"
+                    _hover={{ 
+                        border: "3px solid #63B3ED",
+                    }}
+                >
+                    <Flex
+                        justifyContent="center"
+                    >
+                        <FaCogs
+                            color="#f8f8f7"
+                            fontSize="3rem"
+                        />
+                    </Flex> 
+
+                    <Heading
+                        as="h3"
+                        my="1rem"
+                        color="#f8f8f7"
+                        textAlign="center"
+                        fontSize={["1.5rem", "2rem"]}
+                        fontFamily="Inter, sans-serif"
+                    >
+                        SEO
+                    </Heading>
+
+                    <Text
+                        color="#f8f8f7"
+                        textAlign="center"
+                        fontFamily="Inter, sans-serif"
+                        fontSize={["1.2rem", "1.4rem"]}
+                    >
+                        Estratégias para fazer com que seu site apareça 
+                        cada vez mais nas buscas.
+                    </Text>
+                </Flex>    
+            </Flex> 
+        </Flex>
+    );
+};
 
 export default Servicos;
