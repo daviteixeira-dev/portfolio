6efc279aa24acdc3188ba477edd470606375bc4f
Davi Teixeira <daviteixeira.btm@gmail.com>
2022-08-27 22:00:58 -0300

    componente padrão de card

---
+++ portfolio-v2-chakra-ui/src/components/Card/Card.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { Flex, Heading, Text, Image } from "@chakra-ui/react";
+import LinkButton from "../LinkButton/LinkButton";
+
+function Card(props){
+    return(
+        <Flex
+            mb="1rem"
+            h="30rem"
+            flexDirection="column"
+            w={props.w}
+            //w={["100%", "100%", "49%", "49%"]}
+        >
+            <Image
+                h="15rem"
+                backgroundSize="cover"
+                backgroundImage={props.img}
+                backgroundPosition="center" 
+            />
+
+            <Flex
+                p="1rem"
+                h="10rem"
+                bg="gray.900"
+                alignItems="center"
+                flexDirection="column"
+            >
+                <Heading
+                    as="h3"
+                    color="#f8f8f7"
+                    fontSize="1.5rem"
+                >
+                    {props.title}
+                </Heading>
+
+                <Text
+                    mt="1rem"
+                    color="#f8f8f7"
+                    fontSize="1rem"
+                >
+                    {props.text}
+                </Text>
+            </Flex>
+
+            <Flex
+                h="5rem"
+                p="1.2rem"
+                bg="gray.900"
+                justifyContent="center"
+            >
+                <LinkButton
+                    text="Ver Projeto"
+                    isExternal={true}
+                    link="https://github.com/daviteixeira-btm"
+                />
+            </Flex>
+        </Flex>
+    );
+};
+
+export default Card;
