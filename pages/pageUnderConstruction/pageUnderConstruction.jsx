58fb717de692556b1a08e38cc785198b97ff8331
Davi Teixeira <daviteixeira.btm@gmail.com>
2023-03-10 21:11:47 -0300

    comprimindo as imagens do site

---
+++ pages/pageUnderConstruction/pageUnderConstruction.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container } from '@chakra-ui/react';
+
+const pageUnderConstruction = () => {
+    return (
+        <Container maxW='full' p='0'>
+            <h1>Página em Construção</h1>
+        </Container>
+    );
+};
+
+export default pageUnderConstruction;
