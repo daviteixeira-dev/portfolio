16a3b8e72f4910c2aa86d6372368528f307823c1
Davi Teixeira <daviteixeira.btm@gmail.com>
2023-03-11 16:11:12 -0300

    Teste de um scroll suave

--- src/components/Menus/TopMenu.jsx
+++ src/components/Menus/TopMenu.jsx
@@ -59,7 +59,7 @@ const MenuLinks = () => {
         >
             {
                 menuItems.map(menu => (
-                    <li style={{ listStyle: "none"}}>
+                    <li style={{ listStyle: "none" }}>
                         <a href={`${menu.link}`}>{menu.title}</a>
                     </li>
                 ))
@@ -75,6 +75,7 @@ const NavBarContainer = ({ children, ...props }) => {
             py={4}
             px={8}
             as="nav"
+            className="menu"
             w="100%"
             bg="black"
             zIndex={2}
@@ -94,8 +95,67 @@ const NavBarContainer = ({ children, ...props }) => {
 
 const TopMenu = (props) => {
 
-    return(
-        <NavBarContainer {...props }>
+    const menuItems = document.querySelectorAll('.menu a[href^="#"]')
+
+    menuItems.forEach(item => {
+        item.addEventListener('click', scrollToIdOnClick)
+    })
+
+    function getScrollTopByHref(element) {
+        const id = element.getAttribute('href');
+        return document.querySelector(id).offsetTop;
+    }
+
+    function scrollToIdOnClick(event) {
+        event.preventDefault();
+        const to = getScrollTopByHref(event.target) - 80;
+        scrollToPosition(to)
+    }
+
+    function scrollToPosition(to) {
+        // window.scroll({
+        //     top: to,
+        //     behavior: "smooth",
+        // })
+
+        smoothScrollTo(0, to);
+    }
+
+    /**
+     * Smooth scroll animation
+     * @param {int} endX: destination x coordinate
+     * @param {int} endY: destination y coordinate
+     * @param {int} duration: animation duration in ms
+    */
+
+    function smoothScrollTo(endX, endY, duration) {
+        const startX = window.scrollX || window.pageXOffset;
+        const startY = window.scrollY || window.pageYOffset;
+        const distanceX = endX - startX;
+        const distanceY = endY - startY;
+        const startTime = new Date().getTime();
+
+        duration = typeof duration !== 'undefined' ? duration : 400;
+
+        // Easing function
+        const easeInOutQuart = (time, from, distance, duration) => {
+            if ((time /= duration / 2) < 1) return distance / 2 * time * time * time * time + from;
+            return -distance / 2 * ((time -= 2) * time * time * time - 2) + from;
+        };
+
+        const timer = setInterval(() => {
+            const time = new Date().getTime() - startTime;
+            const newX = easeInOutQuart(time, startX, distanceX, duration);
+            const newY = easeInOutQuart(time, startY, distanceY, duration);
+            if (time >= duration) {
+                clearInterval(timer);
+            }
+            window.scroll(newX, newY);
+        }, 1000 / 60); // 60 fps
+    };
+
+    return (
+        <NavBarContainer {...props}>
             <Logo />
             <MenuLinks />
         </NavBarContainer>
