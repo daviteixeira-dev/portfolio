06c777746db1ae5f011930c90b3ec40f43a8887f
Davi Teixeira <daviteixeira.btm@gmail.com>
2023-08-26 22:39:19 -0300

    logo image path change

--- src/components/DesktopMenu/DesktopMenuLogo.jsx
+++ src/components/DesktopMenu/DesktopMenuLogo.jsx
@@ -3,7 +3,7 @@ import { Flex, Text, Avatar } from "@chakra-ui/react";
 const DesktopMenuLogo = (props) => {
     return (
         <Flex alignItems="center" width={props.size}>
-            <Avatar size="lg" name="Davi Teixeira" src="../../public/images/logo.png"/>
+            <Avatar size="lg" name="Davi Teixeira" src="/images/logo.png"/>
             <Text ml=".5rem" color="#f2f2f2" fontSize="1.5rem">Davi Teixeira</Text>
         </Flex>
     );
