9f5e32cf477a1005f20ccc35d815986aab8b7ae0
Davi Teixeira <daviteixeira.btm@gmail.com>
2023-08-26 22:35:57 -0300

    Creation of the main page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,27 @@
+import { Container } from "@chakra-ui/react";
+
+import Blog from "../components/Blog/Blog";
+import Sobre from "../components/Sobre/Sobre";
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
+import Servicos from "../components/Servicos/Servicos";
+import Projetos from "../components/Projetos/Projetos";
+import Conhecimentos from "../components/Conhecimentos/Conhecimentos";
+import Apresentation from "../components/Apresentation/Apresentation";
+
+const Home = () => {
+    return (
+        <Container maxW="full" className="App" p="0">
+            <Header />
+            <Apresentation />
+            <Sobre />
+            <Servicos />
+            <Conhecimentos />
+            <Projetos />
+            <Blog />
+            <Footer />
+        </Container>
+    );
+};
+
+export default Home;
