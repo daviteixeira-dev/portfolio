9c4a182337f099cca47fc2d7675327fdcb2266d8
Davi Teixeira <daviteixeira.btm@gmail.com>
2023-08-26 22:32:21 -0300

    Header component refactoring

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,93 +1,18 @@
-import { 
-    Flex, 
-    Hide, 
-    Show 
-} from "@chakra-ui/react";
+import { Flex, Hide, Show } from "@chakra-ui/react";
 
-import TopMenu from "../Menus/TopMenu";
-import Presentation from "./Presentation";
-import MenuMobile from "../Menus/MenuMobile";
+import MobileMenu from "../MobileMenu/MobileMenu";
+import DesktopMenu from "../DesktopMenu/DesktopMenu";
 
-function Header(){
-
-    // const menuItems = document.querySelectorAll('.menu a[href^="#"]')
-
-    // menuItems.forEach(item => {
-    //     item.addEventListener('click', scrollToIdOnClick)
-    // })
-
-    // function getScrollTopByHref(element) {
-    //     const id = element.getAttribute('href');
-    //     return document.querySelector(id).offsetTop;
-    // }
-
-    // function scrollToIdOnClick(event) {
-    //     event.preventDefault();
-    //     const to = getScrollTopByHref(event.target) - 80;
-    //     scrollToPosition(to)
-    // }
-
-    // function scrollToPosition(to) {
-    //     // window.scroll({
-    //     //     top: to,
-    //     //     behavior: "smooth",
-    //     // })
-
-    //     smoothScrollTo(0, to);
-    // }
-
-    // /**
-    //  * Smooth scroll animation
-    //  * @param {int} endX: destination x coordinate
-    //  * @param {int} endY: destination y coordinate
-    //  * @param {int} duration: animation duration in ms
-    // */
-
-    // function smoothScrollTo(endX, endY, duration) {
-    //     const startX = window.scrollX || window.pageXOffset;
-    //     const startY = window.scrollY || window.pageYOffset;
-    //     const distanceX = endX - startX;
-    //     const distanceY = endY - startY;
-    //     const startTime = new Date().getTime();
-
-    //     duration = typeof duration !== 'undefined' ? duration : 400;
-
-    //     // Easing function
-    //     const easeInOutQuart = (time, from, distance, duration) => {
-    //         if ((time /= duration / 2) < 1) return distance / 2 * time * time * time * time + from;
-    //         return -distance / 2 * ((time -= 2) * time * time * time - 2) + from;
-    //     };
-
-    //     const timer = setInterval(() => {
-    //         const time = new Date().getTime() - startTime;
-    //         const newX = easeInOutQuart(time, startX, distanceX, duration);
-    //         const newY = easeInOutQuart(time, startY, distanceY, duration);
-    //         if (time >= duration) {
-    //             clearInterval(timer);
-    //         }
-    //         window.scroll(newX, newY);
-    //     }, 1000 / 60); // 60 fps
-    // };
-
+const Header = () => {
     return (
-        <Flex
-            w="100%"
-            as="header"
-            style={{
-                backgroundSize: "cover",
-                backgroundPosition: "10% 30%",
-                backgroundImage: "url(/images/apresentation-banner.jpg)",
-            }}
-        >
+        <Flex as="header" width="100%">
             <Hide below='md'>
-                <TopMenu />
+                <DesktopMenu />
             </Hide>
 
             <Show below='md'>
-                <MenuMobile />
+                <MobileMenu />
             </Show>
-
-            <Presentation />
         </Flex>
     );
 };
