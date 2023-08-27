317bef0a80412b15e9bcfc33fef91bc181d2ecae
Davi Teixeira <daviteixeira.btm@gmail.com>
2023-08-26 22:31:25 -0300

    App component refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,9 @@
-import "./App.css";
-import { Container } from "@chakra-ui/react";
+import Home from "./pages/Home";
 
-import Blog from "./components/Blog/Blog";
-import Sobre from "./components/Sobre/Sobre";
-import Footer from "./components/Footer/Footer";
-import Header from "./components/Header/Header";
-import Projetos from "./components/Projetos/Projetos";
-import Servicos from "./components/Servicos/Servicos";
-import Conhecimentos from "./components/Conhecimentos/Conhecimentos";
-
-function App() {
-  return (
-    <Container maxW="full" className="App" p="0">
-      <Header />
-      <Sobre />
-      <Servicos />
-      <Conhecimentos />
-      <Projetos />
-      <Blog />
-      <Footer />
-    </Container>
-  );
-}
+function App(){
+    return (
+        <Home />
+    );
+};
 
 export default App;
