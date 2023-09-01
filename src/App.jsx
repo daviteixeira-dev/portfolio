86c1ba418fe95fcbbde72537b72972907dfb0afc
Davi Teixeira <daviteixeira.btm@gmail.com>
2023-09-01 19:09:50 -0300

    install the react-router-dom to the project and start the blog page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+
 import Home from "./pages/Home";
+import Blog from './pages/Blog';
 
 function App(){
     return (
-        <Home />
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/blog" element={<Blog />} />
+            </Routes>
+        </BrowserRouter>
     );
 };
 
---
+++ src/pages/Blog.jsx
@@ -0,0 +1,11 @@
+import { Flex, Text } from "@chakra-ui/react";
+
+const Blog = () => {
+    return (
+        <Flex>
+            <Text>Hello World!</Text>
+        </Flex>
+    );
+};
+
+export default Blog;
